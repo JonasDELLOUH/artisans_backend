2ba0e14e826460f79f0a5e6c6beb3c3039c9f1dc
JonasDELLOUH <jdellouh1@gmail.com>
2023-07-27 09:39:19 +0100

    add salon ok1

--- api/controllers/salonController.js
+++ api/controllers/salonController.js
@@ -1,9 +1,20 @@
 import SalonModel from "../models/salonModel.js";
+import JobModel from "../models/jobModel.js";
 
 export const createSalon = async (req, res) => {
     try {
-        const {userId, salonName, lat, long, imageUrl} = req.body;
-        const newSalon = new SalonModel(req.body);
+        const userId = req.headers.userId;
+        const {jobId, salonName, lat, long, imageUrl} = req.body;
+
+        const job = await JobModel.findById(jobId);
+        if (!job) {
+            return res.status(404).json({ message: "ID du job invalide." });
+        }
+
+        const newSalon = new SalonModel({jobId, salonName, lat, long, imageUrl});
+        const salon = await newSalon.save();
+        return res.status(200).json({salon});
+
     } catch (error){
         res.status(409).json({ message: error.message });
     }
--- api/routes/salonRoute.js
+++ api/routes/salonRoute.js
@@ -1,8 +1,9 @@
 import express from "express";
 import {createSalon} from "../controllers/salonController.js";
 import {verifyToken} from "../middlewares/authMiddleware.js";
+import {uploadImage} from "../controllers/storageController.js";
 const router = express.Router();
 
-router.post('/', verifyToken, createSalon);
+router.post('/', verifyToken, uploadImage("imageUrl"), createSalon);
 
 export default router;
