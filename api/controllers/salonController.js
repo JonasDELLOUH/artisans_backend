66bff10c6b0b87ab909440f645e10ab229d24abd
JonasDELLOUH <jdellouh1@gmail.com>
2023-08-16 19:19:12 +0100

    correct salon

--- api/controllers/salonController.js
+++ api/controllers/salonController.js
@@ -1,17 +1,19 @@
 import SalonModel from "../models/salonModel.js";
 import JobModel from "../models/jobModel.js";
+import mongoose from "mongoose";
 
 export const createSalon = async (req, res) => {
     try {
         const userId = req.headers.userId;
-        const {jobId, salonName, lat, long, imageUrl} = req.body;
+        const {jobId, name, lat, long, imageUrl, address, email, phone} = req.body;
+        const totalStar = 25;
 
         const job = await JobModel.findById(jobId);
         if (!job) {
             return res.status(404).json({ message: "ID du job invalide." });
         }
 
-        const newSalon = new SalonModel({jobId, salonName, lat, long, imageUrl});
+        const newSalon = new SalonModel({userId, jobId, name, lat, long, imageUrl, address, email, phone, totalStar});
         const salon = await newSalon.save();
         return res.status(200).json({salon});
 
--- api/models/salonModel.js
+++ api/models/salonModel.js
@@ -2,13 +2,16 @@ import mongoose from "mongoose";
 
 const salonSchema = mongoose.Schema({
         userId: mongoose.Schema.Types.ObjectId,
-        jobId: mongoose.Schema.Types.ObjectId,
-        salonName: {type: String, required: true},
+        jobId: {type: String, required: true},
+        name: {type: String, required: true},
         lat: {type: Number},
         long: {type: Number},
         imageUrl: {type: String},
         address: {type: String},
         nbrStar: {type: Number},
+        totalStar: {type: Number},
+        email: {type: String},
+        phone: {type: String},
     },
     {timestamps: true}
 );
